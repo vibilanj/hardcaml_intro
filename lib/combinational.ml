e45ef38d332ea64bc240d51fe74fefc542c588ba
Vibilan Jayanth <vibilan@firstindo.com>
2024-03-16 21:18:53 +0800

    added test for simple circuit

--- lib/combinational.ml
+++ lib/combinational.ml
@@ -1,16 +1,16 @@
 (* open Hardcaml.Bits *)
 (* open Hardcaml.Signal *)
 
-let vhdl_of_adder_circuit =
+let vhdl_of_adder_circuit () =
   let open Hardcaml.Signal in
   let adder a b = a +: b in
   let c = output "c" (adder (input "a" 8) (input "b" 8)) in
   let circuit = Hardcaml.Circuit.create_exn ~name:"my_adder" [ c ] in
-  Hardcaml.Rtl.print Vhdl circuit
+    Hardcaml.Rtl.print Vhdl circuit
 
 let run () =
   (* let x = of_string "11001" in
      Format.printf "%a@." pp x;
      print_int (width x);
      print_newline () *)
-  vhdl_of_adder_circuit
+  vhdl_of_adder_circuit ()
--- test/hardcaml_intro.ml
+++ test/hardcaml_intro.ml
@@ -1,3 +1,38 @@
-let%expect_test _ =
-  print_endline "Hello, world!";
-  [%expect{| Hello, world! |}]
+let%expect_test "should produce correct vhdl" =
+  Combinational.vhdl_of_adder_circuit ();
+  [%expect {|
+    library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+    entity my_adder is
+        port (
+            b : in std_logic_vector(7 downto 0);
+            a : in std_logic_vector(7 downto 0);
+            c : out std_logic_vector(7 downto 0)
+        );
+    end entity;
+
+    architecture rtl of my_adder is
+
+        -- conversion functions
+        function hc_uns(a : std_logic)        return unsigned         is variable b : unsigned(0 downto 0); begin b(0) := a; return b; end;
+        function hc_uns(a : std_logic_vector) return unsigned         is begin return unsigned(a); end;
+        function hc_sgn(a : std_logic)        return signed           is variable b : signed(0 downto 0); begin b(0) := a; return b; end;
+        function hc_sgn(a : std_logic_vector) return signed           is begin return signed(a); end;
+        function hc_sl (a : std_logic_vector) return std_logic        is begin return a(a'right); end;
+        function hc_sl (a : unsigned)         return std_logic        is begin return a(a'right); end;
+        function hc_sl (a : signed)           return std_logic        is begin return a(a'right); end;
+        function hc_sl (a : boolean)          return std_logic        is begin if a then return '1'; else return '0'; end if; end;
+        function hc_slv(a : std_logic_vector) return std_logic_vector is begin return a; end;
+        function hc_slv(a : unsigned)         return std_logic_vector is begin return std_logic_vector(a); end;
+        function hc_slv(a : signed)           return std_logic_vector is begin return std_logic_vector(a); end;
+        signal hc_4 : std_logic_vector(7 downto 0);
+
+    begin
+
+        hc_4 <= hc_slv(hc_uns(a) + hc_uns(b));
+        c <= hc_4;
+
+    end architecture;
+    |}]
